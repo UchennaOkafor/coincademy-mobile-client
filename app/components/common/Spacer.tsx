58cef937cdc8a6090d61f74c46a953fcf71316cb
Uchenna Okafor <22923123+UchennaOkafor@users.noreply.github.com>
2022-02-12 21:12:24 +0000

    * Refactored the Settings page

--- app/components/common/Spacer.tsx
+++ app/components/common/Spacer.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleProp, View, ViewStyle} from 'react-native';
 import { Theme } from 'styles/Index';
 
 interface Props {
-
+  vertical?: number;
+  horizontal?: number;
 }
 
 const Spacer = (props: Props): JSX.Element => {
+  const defaultSpacing = Theme.spacing.spacingS - Theme.spacing.spacing3XS;
+  const verticalSpacing = props.vertical ?? defaultSpacing;
+  const spacingStyle: StyleProp<ViewStyle> = {
+    marginVertical: verticalSpacing
+  };
+
   return (
-    <View style={{ marginVertical: 10}} />
+    <View style={spacingStyle} />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-
-  }
-});
 
 export default Spacer;
--- app/screens/settings/Settings.tsx
+++ app/screens/settings/Settings.tsx
@@ -4,64 +4,96 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
-	Text
+	Text,
 } from 'react-native';
 import { Theme } from 'styles/Index';
 import { useUserStore } from 'state/useUserStore';
-import { LogOut, Trash2 } from 'react-native-feather';
-import TitleSectionHeader from 'components/headers/TitleSectionHeader';
+import { Book, File, LogOut, Trash2 } from 'react-native-feather';
 import Divider from 'components/common/Divider';
 import Spacer from 'components/common/Spacer';
 import Constants from 'expo-constants';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
+import * as WebBrowser from 'expo-web-browser';
 
 const Settings = (): JSX.Element => {
   const navigation = useNavigation();
   const state = useUserStore();
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Divider />
-        <TitleSectionHeader title='Login' />
-        <TouchableOpacity 
-          style={{ marginTop: 18, flexDirection: 'row', alignItems: 'center'}} 
-          onPress={async () => {
-            await state.logout();
-            navigation.reset({
-              index: 0,
-              routes: [{name: 'Login'}]
-            });
-          }}
-        >
-          <LogOut
-            stroke={Theme.colors.red}
-            width={22}
-            height={22}
-            style={{ marginRight: 15}}
-          />
-          <Text style={{...Theme.typography.text.h6, ...Theme.typography.weight.normal, color: Theme.colors.red }}>Logout</Text>
-        </TouchableOpacity>
+  const loginItems = [{
+    title: 'Logout',
+    icon: LogOut,
+    color: Theme.colors.red,
+    onPress: async () => {
+      await state.logout();
+      navigation.reset({
+        index: 0,
+        routes: [{name: 'Login'}]
+      });
+    }
+  }, {
+      title: 'Clear cache',
+      icon: Trash2,
+      color: Theme.colors.gray,
+      onPress: async () => {
+        await state.clearCache();
+        navigation.reset({
+          index: 0,
+          routes: [{name: 'Onboarding'}]
+        });
+      }
+    }
+  ];
 
-        <TouchableOpacity 
-          style={{ marginTop: 18, flexDirection: 'row', alignItems: 'center'}} 
-          onPress={async () => {
-            await state.clearCache();
-            navigation.reset({
-              index: 0,
-              routes: [{name: 'Onboarding'}]
-            });
-          }}
-        >
-          <Trash2
-            stroke={Theme.colors.gray}
-            width={22}
-            height={22}
-            style={{ marginRight: 15}}
-          />
-          <Text style={{...Theme.typography.text.h6, ...Theme.typography.weight.normal, color: Theme.colors.gray }}>Clear cache</Text>
-        </TouchableOpacity>
-      </View>
+  const aboutItems = [{
+    title: 'Privacy Policy',
+    icon: File,
+    color: Theme.colors.grayDark,
+    onPress: () => WebBrowser.openBrowserAsync('https://expo.dev')
+  }, {
+      title: 'Terms of Service',
+      icon: Book,
+      color: Theme.colors.grayDark,
+      onPress: () => WebBrowser.openBrowserAsync('https://expo.dev')
+    }
+  ];
+
+  const sections = [
+    {title: 'About', items: aboutItems},
+    {title: 'Auth', items: loginItems},
+  ];
+
+  return (
+    <SafeAreaView style={styles.container} edges={['bottom']}>
+      <ScrollView>
+        <Spacer />
+        {sections.map((item) => {
+          return (
+            <View key={item.title}>
+              <Text style={{...Theme.typography.text.h6, ...Theme.typography.weight.medium, color: Theme.colors.grayDark900 }}>{item.title.toUpperCase()}</Text>
+              <Spacer />
+              <Divider />
+              {item.items.map((item) => {
+                return (
+                  <TouchableOpacity
+                    key={item.title}
+                    style={{ marginTop: 18, flexDirection: 'row', alignItems: 'center'}} 
+                    onPress={item.onPress}>
+                    <item.icon
+                      stroke={item.color}
+                      width={22}
+                      height={22}
+                      style={{ marginRight: 15}}
+                    />
+                    <Text style={{...Theme.typography.text.h6, ...Theme.typography.weight.normal, color: item.color }}>{item.title}</Text>
+                  </TouchableOpacity>
+                )
+              })}
+              <Spacer vertical={Theme.spacing.spacingL} />
+            </View>
+          )
+        })}
+      </ScrollView>
       <View style={{ alignSelf: 'center', marginBottom: 20}}>
         <Text style={{ ...Theme.typography.text.h7, ...Theme.typography.weight.normal, color: Theme.colors.gray}}>v{Constants.manifest?.version}</Text>
       </View>
