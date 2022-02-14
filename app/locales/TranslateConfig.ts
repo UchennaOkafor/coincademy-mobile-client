ea50ac85758af964c2dffc7d40974f019f1463bc
Uchenna Okafor <22923123+UchennaOkafor@users.noreply.github.com>
2022-02-14 00:56:44 +0000

    * Added localization dependency

---
+++ app/locales/TranslateConfig.ts
@@ -0,0 +1,11 @@
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+// Set the key-value pairs for the different languages you want to support.
+i18n.translations = {
+	en: require('@locales/en.json'),
+	ja: { welcome: 'こんにちは' },
+};
+
+// Set the locale once at the beginning of your app.
+i18n.locale = Localization.locale;
